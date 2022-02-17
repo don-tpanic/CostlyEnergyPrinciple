28d986190611ac5d4b44b91cf592339f2010c94d
Xiaoliang (Ken) Luo <ken@love20.pals.ucl.ac.uk>
2022-02-17 20:14:02 +0000

    change code: use un-inhibted cluster targets

--- models.py
+++ models.py
@@ -379,7 +379,8 @@ class JointModel(Model):
             totalSupport = totalSupport / tf.reduce_sum(clusters_actv_softmax)
             print(f'[Check] totalSupport = {totalSupport}')
             
-        return inputs_binary, clusters_actv_softmax, y_pred, totalSupport
+        # return inputs_binary, clusters_actv_softmax, y_pred, totalSupport
+        return inputs_binary, clusters_actv, y_pred, totalSupport
 
 
 if __name__ == '__main__':

06fd7691d82a8d236f9c73394302b591254e0d85
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-05-04 16:02:41 +0100

    clean code

--- config_combiner.py
+++ config_combiner.py
@@ -12,35 +12,37 @@ Impl:
 -----
 """
 
-joint_model_config_version = 'v0_naive-withNoise'
+def merge(joint_model_config_version):
 
-num_subs = 23 
-subs = [f'{i:02d}' for i in range(2, num_subs+2) if i!=9]
-num_subs = len(subs)
+    num_subs = 23 
+    subs = [f'{i:02d}' for i in range(2, num_subs+2) if i!=9]
+    num_subs = len(subs)
 
-joint_config = load_config(
-    component=None, 
-    config_version=joint_model_config_version
-)
-joint_config['clustering_config_version'] = ''
-joint_keys = joint_config.keys()
+    joint_config = load_config(
+        component=None, 
+        config_version=joint_model_config_version
+    )
+    joint_config['clustering_config_version'] = ''
+    joint_keys = joint_config.keys()
 
-for sub in subs:
-    config_version = f'best_config_sub{sub}'
-    config = load_config(
-        component='clustering',
-        config_version=config_version)
-    config_keys = config.keys()
-    
-    for key in config_keys:
-        if key == 'config_version':
-            joint_config[f'clustering_{key}'] = config[key]
-        else:
-            joint_config[key] = config[key]
-    
-    
-    filepath = os.path.join(f'configs', f'config_{config_version}.yaml')
-    with open(filepath, 'w') as yaml_file:
-        yaml.dump(joint_config, yaml_file, default_flow_style=False)
+    for sub in subs:
+        config_version = f'best_config_sub{sub}'
+        config = load_config(
+            component='clustering',
+            config_version=config_version)
+        config_keys = config.keys()
         
-    exit()
+        for key in config_keys:
+            if key == 'config_version':
+                joint_config[f'clustering_{key}'] = config[key]
+            else:
+                joint_config[key] = config[key]
+        
+        filepath = os.path.join(f'configs', f'config_{config_version}.yaml')
+        with open(filepath, 'w') as yaml_file:
+            yaml.dump(joint_config, yaml_file, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    joint_model_config_version = 'v4_naive-withNoise'
+    merge(joint_model_config_version)

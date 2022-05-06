42aeababd7403df06f95050830f2e4be4f711652
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-05-06 18:11:38 +0100

    add `noise_level` in search

--- config_generator.py
+++ config_generator.py
@@ -11,8 +11,9 @@ def hyperparams_ranges():
     lr_attn_ = [0.00092, 0.0092, 0.092]
     inner_loop_epochs_ = [5, 10, 15, 20]
     recon_clusters_weighting_ = [1000, 10000, 100000, 1000000]
+    noise_level_ = [0.3, 0.4, 0.5]
     
-    return lr_attn_, inner_loop_epochs_, recon_clusters_weighting_
+    return lr_attn_, inner_loop_epochs_, recon_clusters_weighting_, noise_level_
 
 
 def per_subject_generate_candidate_configs(ct, v, sub, template_version='v4_naive-withNoise'):
@@ -43,21 +44,24 @@ def per_subject_generate_candidate_configs(ct, v, sub, template_version='v4_naiv
     # TODO: this could be later adjusted to per sub
     lr_attn_, \
         inner_loop_epochs_, \
-            recon_clusters_weighting_ = hyperparams_ranges()
+            recon_clusters_weighting_, \
+                noise_level_ = hyperparams_ranges()
 
     # update all low_attn entries in template
     for lr_attn in lr_attn_:
         for inner_loop_epochs in inner_loop_epochs_:
             for recon_clusters_weighting in recon_clusters_weighting_:
-                config_version = f'hyper{ct}_sub{sub}_{v}'
-                template['lr_attn'] = lr_attn
-                template['inner_loop_epochs'] = inner_loop_epochs
-                template['recon_clusters_weighting'] = recon_clusters_weighting
+                for noise_level in noise_level_:
+                    config_version = f'hyper{ct}_sub{sub}_{v}'
+                    template['lr_attn'] = lr_attn
+                    template['inner_loop_epochs'] = inner_loop_epochs
+                    template['recon_clusters_weighting'] = recon_clusters_weighting
+                    template['noise_level'] = noise_level
 
-                filepath = os.path.join(f'configs', f'config_{config_version}.yaml')
-                with open(filepath, 'w') as yaml_file:
-                    yaml.dump(template, yaml_file, default_flow_style=False)
-                ct += 1
+                    filepath = os.path.join(f'configs', f'config_{config_version}.yaml')
+                    with open(filepath, 'w') as yaml_file:
+                        yaml.dump(template, yaml_file, default_flow_style=False)
+                    ct += 1
                                 
     print(f'Total number of candidates = {ct}')
 

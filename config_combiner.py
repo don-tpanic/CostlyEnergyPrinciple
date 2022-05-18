840e1b8c272124000bca62964bf52736fec379dd
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-05-18 15:32:10 +0100

    change code: combine best config from clustering to best config for joint_model

--- config_combiner.py
+++ config_combiner.py
@@ -3,46 +3,60 @@ import yaml
 from utils import load_config
 
 """
-Given the best config from `clustering/configs/`
-combine it with config for the joint_model from `configs/`
-to produce top-level best_configs that has all variables 
-for both clustering and sustain module in the joint model.
+We search hyper-params of the joint_model in two separate stages.
+First, we search hyper-params for clustering module for each subject,
+independent to DCNN. 
 
-Impl:
------
+Second, we freeze clustering hyper-params in clustering module, and 
+search for hyper-params in DCNN.
+
+Therefore, after we search for clustering module, we need to carry
+over the best configs into configs of the joint_model so that we could
+start the second stage search. 
+
+This script does the `carryover` by first loading the current best config
+of the joint_model (with out dated hyper-params for clustering), then we 
+load the best configs of clustering and we substitute the hyper-params 
+of the clustering configs into the joint_model configs. This way, 
+the joint_model configs will have best configs from clustering and could 
+continue to search hyper-params for DCNN.
 """
 
-def merge(joint_model_config_version):
-
+def merge():
     num_subs = 23 
     subs = [f'{i:02d}' for i in range(2, num_subs+2) if i!=9]
     num_subs = len(subs)
 
-    joint_config = load_config(
-        component=None, 
-        config_version=joint_model_config_version
-    )
-    joint_config['clustering_config_version'] = ''
-    joint_keys = joint_config.keys()
-
     for sub in subs:
         config_version = f'best_config_sub{sub}'
+        
+        # get the current joint_model best config
+        # subject-specific
+        joint_config = load_config(
+            component=None, 
+            config_version=config_version)
+        joint_config['clustering_config_version'] = ''
+        
+        # load the current best clustering model config
+        # (clustering params not update-to-date)
         config = load_config(
             component='clustering',
             config_version=config_version)
         config_keys = config.keys()
         
+        # sub clustering model params into
+        # the joint_model config
         for key in config_keys:
             if key == 'config_version':
                 joint_config[f'clustering_{key}'] = config[key]
             else:
                 joint_config[key] = config[key]
         
+        # save the best config overall.
         filepath = os.path.join(f'configs', f'config_{config_version}.yaml')
         with open(filepath, 'w') as yaml_file:
             yaml.dump(joint_config, yaml_file, default_flow_style=False)
 
 
 if __name__ == '__main__':
-    joint_model_config_version = 'v4_naive-withNoise'
-    merge(joint_model_config_version)
+    merge()

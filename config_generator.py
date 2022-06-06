2171a6dd1935720542f1a132042e682b0f4a701e
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2022-06-06 16:17:38 +0100

    fix mistake: `reg_strength` should have been `high_attn_reg_strength`

--- config_generator.py
+++ config_generator.py
@@ -19,7 +19,7 @@ def per_subject_hyperparams_ranges(sub, v):
     thr = config['thr']
     beta = config['beta']
     temp2 = config['temp2']
-    reg_strength = config['reg_strength']
+    high_attn_reg_strength = config['high_attn_reg_strength']
     lr_attn = config['lr_attn']
     inner_loop_epochs = config['inner_loop_epochs']
     recon_clusters_weighting = config['recon_clusters_weighting']
@@ -57,10 +57,10 @@ def per_subject_hyperparams_ranges(sub, v):
         temp2,
     ]
         
-    reg_strength_ = [
-        reg_strength*0.75,
-        reg_strength,
-        reg_strength*1.5, 
+    high_attn_reg_strength_ = [
+        high_attn_reg_strength*0.75,
+        high_attn_reg_strength,
+        high_attn_reg_strength*1.5
     ]
     
     lr_attn_ = [
@@ -88,7 +88,7 @@ def per_subject_hyperparams_ranges(sub, v):
     ]
         
     return lr_, attn_lr_multiplier_, \
-                Phi_, specificity_, thr_, beta_, temp2_, reg_strength_, \
+                Phi_, specificity_, thr_, beta_, temp2_, high_attn_reg_strength_, \
                     lr_attn_, inner_loop_epochs_, recon_clusters_weighting_, noise_level_
                         
 
@@ -138,14 +138,14 @@ def per_subject_generate_candidate_configs(ct, v, sub):
     #             noise_level_ = hyperparams_ranges()
     
     lr_, attn_lr_multiplier_, \
-        Phi_, specificity_, thr_, beta_, temp2_, reg_strength_, \
+        Phi_, specificity_, thr_, beta_, temp2_, high_attn_reg_strength_, \
             lr_attn_, inner_loop_epochs_, recon_clusters_weighting_, noise_level_ = \
                 per_subject_hyperparams_ranges(sub=sub, v=v)
     
     # update all low_attn entries in template
     for lr in lr_:
         for attn_lr_multiplier in attn_lr_multiplier_:
-            for reg_strength in reg_strength_:
+            for high_attn_reg_strength in high_attn_reg_strength_:
                 for lr_attn in lr_attn_:
                     for inner_loop_epochs in inner_loop_epochs_:
                         for recon_clusters_weighting in recon_clusters_weighting_:
@@ -153,7 +153,7 @@ def per_subject_generate_candidate_configs(ct, v, sub):
                                 config_version = f'hyper{ct}_sub{sub}_{v}'
                                 template['lr'] = lr
                                 template['attn_lr_multiplier'] = attn_lr_multiplier
-                                template['reg_strength'] = reg_strength
+                                template['high_attn_reg_strength'] = high_attn_reg_strength
                                 template['lr_attn'] = lr_attn
                                 template['inner_loop_epochs'] = inner_loop_epochs
                                 template['recon_clusters_weighting'] = recon_clusters_weighting
@@ -172,7 +172,7 @@ if __name__ == '__main__':
     subs = [f'{i:02d}' for i in range(2, num_subs+2) if i!=9]
     for sub in subs:
         per_subject_generate_candidate_configs(
-            ct=525, v='fit-human-entropy-fast', sub=sub
+            ct=2712, v='fit-human-entropy-fast', sub=sub
         )
         
     # [0, 525)
@@ -182,3 +182,4 @@ if __name__ == '__main__':
             # noise_level_ = [0.2, 0.3, 0.4, 0.5, 0.6]
     
     # [525, 2712)
+            # high_attn_reg_strength was not searched (was searching reg_strength which is not needed)
